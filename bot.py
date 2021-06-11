3ee2094ff0213dda483efecf1ae42cd1b09eaf79
Navindu Dilmin <74837844+Naviya2@users.noreply.github.com>
2021-06-11 21:14:05 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,21 +10,21 @@ from telethon.tl.functions.users import GetFullUserRequest
 logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s', level=logging.WARNING)
 
 # start the bot
-print("Starting...")
+print("Now Starting Leo View Counter Bot")
 try:
     apiid = config("APP_ID", cast=int)
     apihash = config("API_HASH")
     bottoken = config("BOT_TOKEN")
     FRWD_CHANNEL = config("FRWD_CHANNEL", cast=int)
-    LeoProjects = TelegramClient('leosupportx', apiid, apihash).start(bot_token=bottoken)
+    LeoViewCounterBot = TelegramClient('LeoViewCounterBot', apiid, apihash).start(bot_token=bottoken)
 except:
     print("Environment vars are missing! Kindly recheck.")
     print("Bot is quiting...")
     exit()
 
-@BotzHub.on(events.NewMessage(pattern="/start", func=lambda e: e.is_private))
+@LeoViewCounterBot.on(events.NewMessage(pattern="/start", func=lambda e: e.is_private))
 async def _(event):
-    ok = await BotzHub(GetFullUserRequest(event.sender_id))
+    ok = await LeoViewCounterBot(GetFullUserRequest(event.sender_id))
     await event.reply(f"Hello {ok.user.first_name}👋 \nI'm a Leo View Counter Bot 🇱🇰\nSend me a message and I'll attach a view count to it 🙂",
                     buttons=[
                         [Button.url("Developer🧑‍💻", url="https://t.me/naviya2"),
@@ -34,7 +34,7 @@ async def _(event):
                         Button.url("Support Group👥", url="https://t.me/leosupportx")]
                     ])
 
-@BotzHub.on(events.NewMessage(incoming=True, func=lambda e: e.is_private))
+@LeoViewCounterBot.on(events.NewMessage(incoming=True, func=lambda e: e.is_private))
 async def countit(event):
     if event.text.startswith('/'):
         return
@@ -43,4 +43,4 @@ async def countit(event):
 
 print("Leo View Counter Bot is Started")
 print("Do visit @new_ehi..")
-BotzHub.run_until_disconnected()
+LeoViewCounterBot.run_until_disconnected()
