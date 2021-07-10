9f6b0631c9f6ab31d63f34b48b0ea3b88b6eddc5
Navindu Dilmin <74837844+Naviya2@users.noreply.github.com>
2021-07-10 23:33:30 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -6,7 +6,7 @@ import os
 
 class Config(object):
     BOT_USERNAME = os.environ.get("BOT_USERNAME")
-    SESSION_NAME = os.environ.get("SESSION_NAME", "Rename-Bot-0")
+    SESSION_NAME = os.environ.get("SESSION_NAME", "Leo_View_Counter_Bot")
     BOT_OWNER = int(os.environ.get("BOT_OWNER", 1069002447))
     UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", None)
     LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", -100))
