3dc2aa5e8bc392ad0771bada67e88f6936e5207d
Navindu Dilmin <74837844+Naviya2@users.noreply.github.com>
2021-07-12 11:14:51 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -37,8 +37,8 @@ except:
 
 @LeoViewCounterBot.on(events.NewMessage(pattern="/start", func=lambda e: e.is_private))
 async def _(event):
-    await AddUserToDatabase(event)
-    FSub = await ForceSub(event)
+    await AddUserToDatabase(client, message)
+    FSub = await ForceSub(client, message)
     if FSub == 400:
         return
     ok = await LeoViewCounterBot(GetFullUserRequest(event.sender_id))
@@ -53,8 +53,8 @@ async def _(event):
 
 @LeoViewCounterBot.on(events.NewMessage(incoming=True, func=lambda e: e.is_private))
 async def countit(event):
-    await AddUserToDatabase(event)
-    FSub = await ForceSub(event)
+    await AddUserToDatabase(client, message)
+    FSub = await ForceSub(client, message)
     if FSub == 400:
         return
     if event.text.startswith('/'):
